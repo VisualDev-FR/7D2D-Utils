c5d1f823bf0af86e9ca66ef4fbc13e8219292c38
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-07-12 23:28:12 +0200

    update starter modApi.cs

--- _starter/ModApi.cs
+++ _starter/ModApi.cs
@@ -1,13 +1,10 @@
 using System.Reflection;
 
-namespace Harmony
+public class ModAPI : IModApi
 {
-    public class @MODNAME!pascal : IModApi
+    public void InitMod(Mod _modInstance)
     {
-        public void InitMod(Mod _modInstance)
-        {
-            var harmony = new HarmonyLib.Harmony(_modInstance.Name);
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
+        var harmony = new HarmonyLib.Harmony(_modInstance.Name);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
     }
 }
